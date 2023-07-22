1bc939526f1b64f6bd2c770a3c34e1be07661a4a
Abdirahman Jama <mustalid21@gmail.com>
2023-07-22 14:42:55 -0500

    finished assignment

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,17 +1,18 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
-let firstName = 'Abdirahman ';
+let firstName = 'Abdirahman';
 console.log( firstName );
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
 let lastName = 'Jama'
 console.log( lastName );;
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
-let fullName = firstName + lastName ;
+let fullName = firstName + '' + lastName ;
 // 4 - Console log the value of `fullName`
 console.log( fullName );
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
 let luckyNumber = 7
+console.log(luckyNumber);
 // 6 - Console log this sentence, adding in the variables you created above: 
 // 'My name is (full name), and I think (lucky number) is a winner!'.
 // Refer back to the videos if you need help with this one.
@@ -40,7 +41,7 @@ if (adventurous === true) {console.log( 'Adventures are great!'); //end adventur
 if ( adventurous != true ) { console.log( 'How about we stay home?');}
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
-if ( luckyNumber = 2 && adventurous === true) { console.log( 'Roll the dice! ');}
+if ( luckyNumber === 2 && adventurous === true) { console.log( 'Roll the dice! ');}
 // 15 - Write a conditional that console logs "I can have more pets!" 
 //if the value of `pets` is less than the value of `allowedPets`,
 if (pets < allowedPets) {console.log('I can have more pets!');
@@ -96,7 +97,27 @@ if (pets > friendsPets) {console.log('mostPets:', + pets);
 //      "Third is the one with the polka dot dress" if your lucky number is 3
 //      Otherwise, log "Luck is what happens when preparation meets opportunity"
 //      You'll need to research how to use switch statements!
+switch (luckyNumber) {
+    case 1: 
+        console.log('First is the worst');
+        break;
+    case 2: 
+        console.log('Second is the best');
+    case 3: 
+        console.log('Third is the one with the polka dot dress');
+        break;
+    default:
+        console.log('"Luck is what happens when preparation meets opportunity"');
+        break;
+}
 
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
 
+// 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
+//if (adventurous === true) {console.log( 'Adventures are great!'); //end adventures are great    
+//} else { console.log('Adventures are not great');}
 
+
+let display = 
+    (adventurous === true) ? ('Adventures are great!') : ('Adventures are not great.')
+    console.log(display);
